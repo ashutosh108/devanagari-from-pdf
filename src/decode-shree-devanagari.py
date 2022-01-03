b08161119d616ba347500ef28cb99eda3c1d43ce
Anton Voloshin <ashutosh108@gmail.com>
2022-01-03 09:32:02 +0700

    refactoring and minor optimizations

--- src/decode-shree-devanagari.py
+++ src/decode-shree-devanagari.py
@@ -107,106 +107,135 @@ def add_r_before_rules(dic):
 			new[new_from] = new_to
 	return {**new, **dic}
 
+def add_t_before_rules(dic):
+	new = {}
+	for f, t in dic.items():
+		if t[-1] in "aieou":
+			new_from = 'O' + f
+			new_to = 't' + t
+			new[new_from] = new_to
+	return {**new, **dic}
+
+def add_ssh_before_rules(dic):
+	new = {}
+	for f, t in dic.items():
+		if t[-1] in "aieou":
+			new_from = ']' + f
+			new_to = '"s' + t
+			new[new_from] = new_to
+	return {**new, **dic}
+
+def add_s_before_rules(dic):
+	new = {}
+	for f, t in dic.items():
+		if t[-1] in "aieou":
+			new_from = '_' + f
+			new_to = 's' + t
+			new[new_from] = new_to
+	return {**new, **dic}
+
+repl = {
+	'@°':		'ka',
+	'B"':		'ga',
+	'E"':		'ca',
+	'G"':		'ja',
+	'N"':		'.na',
+	'd':		'"sra',
+	'e"':		'tra',
+	'ﬂ"':		'nna',
+	'}œ"':		'hma',
+	'◊O"':		'kta',
+	'_O"':		'sta',
+	'SO"':		'nta',
+	'O"':		'ta',
+	'P"':		'tha',
+	'SQ':		'nda',
+	'Q':		'da',
+	'_"':		'sa',
+	'—"':		'tna',
+	'S"':		'na',
+	'_\\"':		'sva',
+	'\\"':		'va',
+	'V"':		'bra',
+	'W"':		'bha',
+	'X"':		'ma',
+	'\\Y"':		'vya',
+	'Y\\"':		'yva',
+	'_OY"':		'stya',
+	'SY"':		'nya',
+	'_Y"':		'sya',
+	'Y"':		'ya',
+	'Z':		'ra',
+	']"':		'"sa',
+	'[':		'la',
+	'c"':		'j~na',
+	'T"}':		'pra',
+	'T"n':		'pna',
+	'T"':		'pa',
+	'^"':		'.sa',
+	'ù':		'dya',
+	'b"':		'k.sa',
+	'z':		'.m',
+	'Õ>':		'.s.ta',
+	'Ç"':		'cca',
+	'∆"':		'"sca',
+	'R"':		'dha',
+	'ä':		'"nka',
+	'qwe':		'qwe'
+}
+repl = add_virama_rules(repl)
+repl = add_long_a_rules(repl)
+repl = add_u_rules(repl)
+repl = add_long_u_rules(repl)
+repl = add_o_rules(repl)
+repl = add_au_rules(repl)
+repl = add_i_rules(repl)
+repl = add_long_i_rules(repl)
+repl = add_e_rules(repl)
+repl = add_ai_rules(repl)
+repl = add_t_before_rules(repl)
+repl = add_r_before_rules(repl)
+repl = add_ssh_before_rules(repl)
+repl = add_s_before_rules(repl)
+repl = add_unchanging_letters(repl)
+
+# avoid replicating these special rules to "aa", "ii", halant etc
+repl['&'] = '.a'
+repl['·°'] = 'ruu'
+repl['ë'] = '‘'; # U+2018 left single quotation mark
+repl['í'] = '’'; # U+2019 right single quotation mark
+repl['Ïp'] = 'aa';
+repl['Ï'] = 'a';
+repl['$'] = '|';
+repl['#'] = '.h';
+repl['Ú'] = 'i';
+repl['ñ'] = '—';
+
+repl_for_letter = {}
+
+for k, v in repl.items():
+	if k[0] in repl_for_letter:
+		repl_for_letter[k[0]][k] = v
+	else:
+		repl_for_letter[k[0]] = {k: v}
+
 def decodeline(line):
-	repl = {
-		'$':		'|',
-		'B"':		'ga',
-		'd':		'"sra',
-		'e"':		'tra',
-		'ﬂ"':		'nna',
-		'#':		'.h',
-		'Ú':		'i',
-		'ñ':		'—',
-		'}œ"':		'hma',
-		'◊O"':		'kta',
-		'_O"':		'sta',
-		'SO"':		'nta',
-		'O"':		'ta',
-		'O_"':		'tsa',
-		'O\\"':		'tva',
-		'P"':		'tha',
-		'SQ':		'nda',
-		'Q':		'da',
-		'_"':		'sa',
-		'OS"':		'tna',
-		'—"':		'tna',
-		']S"':		'"sna',
-		'S"':		'na',
-		'_\\"':		'sva',
-		'\\"':		'va',
-		'V"':		'bra',
-		'W"':		'bha',
-		'OX"':		'tma',
-		'X"':		'ma',
-		'\\Y"':		'vya',
-		'Y\\"':		'yva',
-		'_OY"':		'stya',
-		'SY"':		'nya',
-		'_Y"':		'sya',
-		'OY"':		'tya',
-		']Y"':		'"sya',
-		'Y"':		'ya',
-		'Z':		'ra',
-		'@°':		'ka',
-		']"':		'"sa',
-		'[':		'la',
-		'c"':		'j~na',
-		'T"}':		'pra',
-		'T"n':		'pna',
-		'T"':		'pa',
-		'^"':		'.sa',
-		'ù':		'dya',
-		'b"':		'k.sa',
-		'z':		'.m',
-		'Õ>':		'.s.ta',
-		'N"':		'.na',
-		'Ç"':		'cca',
-		'∆"':		'"sca',
-		'G"':		'ja',
-		'E"':		'ca',
-		'R"':		'dha',
-		'ä':		'"nka',
-		'qwe':		'qwe'
-	}
-	repl = add_unchanging_letters(repl)
-	repl = add_virama_rules(repl)
-	repl = add_long_a_rules(repl)
-	repl = add_u_rules(repl)
-	repl = add_long_u_rules(repl)
-	repl = add_o_rules(repl)
-	repl = add_au_rules(repl)
-	repl = add_i_rules(repl)
-	repl = add_long_i_rules(repl)
-	repl = add_e_rules(repl)
-	repl = add_ai_rules(repl)
-	repl = add_r_before_rules(repl)
-
-	# avoid replicating these special rules to "aa", "ii", halant etc
-	repl['&'] = '.a'
-	repl['·°'] = 'ruu'
-	repl['ë'] = '‘'; # U+2018 left single quotation mark
-	repl['í'] = '’'; # U+2019 right single quotation mark
-	repl['Ïp'] = 'aa';
-	repl['Ï'] = 'a';
-
 	res = ''
 
-
-	class ContinueLine(Exception):
-		pass
-
-	while len(line) != 0:
-		try:
-			for repl_from, repl_to in repl.items():
+	while line:
+		continue2 = False
+		if line[0] in repl_for_letter:
+			for repl_from, repl_to in repl_for_letter[line[0]].items():
 				if line.startswith(repl_from):
 					res += repl_to
 					line = line[len(repl_from):]
-					raise ContinueLine
-			# could not find any known prefix, escape next char
-			res += "[" + line[0] + "]"
-			line = line[1:]
-		except ContinueLine:
+					continue2 = True
+					break
+		if continue2:
 			continue
+		# could not find any known prefix, escape next char
+		res += "[" + line[0] + "]"
+		line = line[1:]
 	return res
 
 def main(args):
