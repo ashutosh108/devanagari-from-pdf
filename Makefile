0371bbca86c98cf5100b7eec4fea3481d271739b
Anton Voloshin <ashutosh108@gmail.com>
2022-01-04 14:30:08 +0700

    improve r-/-r handling. Add sra, p-. Lines 20, 21 work.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 # we use bash for <(cmd) argument to diff
 SHELL := bash
-TESTS := 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+TESTS := 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
 
 test: sample/p113.txt FORCE
 	@test/test-line p113 ${TESTS}
--- src/decode-shree-devanagari.py
+++ src/decode-shree-devanagari.py
@@ -38,6 +38,7 @@ repl = {
 	'´':		'ddha',
 	'ü':		'dva',
 	'_"':		'sa',
+	'æ':		'sra',
 	'—"':		'tna',
 	'S"':		'na',
 	'\\"':		'va',
@@ -88,6 +89,7 @@ repl_prefix = {
 	']':	'"s',
 	'_':	's',
 	'S':	'n',
+	'T':	'p',
 	'X':	'm',
 	'\\':	'v',
 	'Y':	'y',
@@ -95,37 +97,20 @@ repl_prefix = {
 	'◊':	'k'
 }
 
-# trailing letters lookup. Must be longest-first among matching prefixes since
+# trailing vowels lookup. Must be longest-first among matching prefixes since
 # first match wins and we want the longest one among the two entries to win.
-# Almost every entry is duplicated with it's "prefix r" version. Later We test
-# if the last symbol of the key is '{' to add 'r' in front of syllable.
-#
-# '{' (r-before-syllable) is a special case because it doesn't need
-# corresponding "short a" entry
-#
-# '}' means *trailing* r
+# e.g. 'pv' must go before 'p'.
 
 repl_trailing = {
-	'pu{':	'o',
 	'pu':	'o',
-	'pv{':	'au',
 	'pv':	'au',
-	'p{':	'aa',
 	'p':	'aa',
-	'v{':	'ai',
 	'v':	'ai',
-	'r{':	'ii',
 	'r':	'ii',
-	's{':	'u',
 	's':	'u',
 	'l':	'u',
-	't{':	'uu',
 	't':	'uu',
-	'u{':	'e',
-	'u':	'e',
-	'{':	'a',
-	'}r':	'rii',
-	'}':	'ra',
+	'u':	'e'
 }
 
 # optimization: group all entries by first char of it's key to make linear
@@ -146,19 +131,36 @@ def handle_i_modifier(i_modifier, repl_to):
 	i_modifier = False
 	return i_modifier, repl_to
 
+def add_before_last_vowel(what, syllable):
+	vowels=''
+	while syllable[-1:] in "aeiou":
+		vowels += syllable[-1:]
+		syllable = syllable[:-1]
+	return syllable + what + vowels
+
 def handle_trailing_vowels_and_r(line, repl_to):
-	got3 = line[0:3] in repl_trailing
-	got2 = line[0:2] in repl_trailing
-	if got3 or got2 or line[0:1] in repl_trailing:
-		from_trailing = line[0:3] if got3 else line[0:2] if got2 else line[0:1]
-		if repl_to.endswith('a'):
+	while True:
+		got3 = line[0:3] in repl_trailing
+		got2 = line[0:2] in repl_trailing
+		if got3 or got2 or line[0:1] in repl_trailing:
+			from_trailing = line[0:3] if got3 else line[0:2] if got2 else line[0:1]
+			if not repl_to[-1] in "ai":
+				repl_to = "[WARNING: '%s' modifier after a syllable ending not on 'a': %s]" % (from_trailing, repl_to) + repl_to
 			repl_to = repl_to[:-1] + repl_trailing[from_trailing]
 			# special case: add 'r' *before* the syllable
 			if from_trailing[-1] == '{':
 				repl_to = 'r' + repl_to
 			line = line[len(from_trailing):]
+		# add frontal "r" as in rvi, rva
+		elif line[0:1] == '{':
+			repl_to = 'r' + repl_to
+			line = line[1:]
+		# add trailing "r" as in grii, gra
+		elif line[0:1] == '}':
+			repl_to = add_before_last_vowel('r', repl_to)
+			line = line[1:]
 		else:
-			raise Exception("'%s' modifier after a syllable ending not on 'a': %s" % (from_trailing, repl_to))
+			break
 	return line, repl_to
 
 def decodeline(line):
